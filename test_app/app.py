71a6300e4c92e69eb982498224a270749f566d3a
DaniilAnichin <anichindaniil@gmail.com>
2019-12-15 21:23:41 +0200

    Finish view, other modes & so on

--- test_app/app.py
+++ test_app/app.py
@@ -4,13 +4,15 @@ def tests():
     test_item = Item.query.filter_by(item_type='test').first()
     return render_template('item.html',  item=test_item)
 """
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_nav.elements import Navbar, View
+import tempfile
 
-from .admin_views import admin
+from flask import Blueprint, render_template, request, redirect, url_for, flash, send_file
+from flask_nav.elements import Navbar, View
+from werkzeug.datastructures import CombinedMultiDict
+
 from .forms import *
-from .setup import app, db, nav
-from .models import *
+from .setup import app, nav
+from .ciphers import Ciphers, Deencrypter, Modes, default_key
 
 
 test_app = Blueprint('test_app', __name__)
@@ -30,9 +32,46 @@ def index():
     return render_template('index.html')
 
 
+def process_form(form):
+    if form.file_name.data:
+        content = form.file_name.data.read()
+    else:
+        content = form.raw_text.data
+
+    if not (form.file_name.data or form.raw_text.data):
+        flash(f'Either raw text or filename should be passed', 'error')
+        return None
+
+    if form.key.data:
+        key = form.key.data
+    else:
+        key = default_key()
+
+    if not form.mode.raw_data:
+        flash(f'Need to choose mode', 'error')
+        return None
+    if not form.cipher.raw_data:
+        flash(f'Need to choose cipher', 'error')
+        return None
+
+    mode = Modes(form.mode.data)
+    cipher = Ciphers(form.cipher.data)
+    reverse = form.decrypt.data
+    dnc = Deencrypter(cipher, mode, key)
+    return dnc.decrypt(content) if reverse else dnc.encrypt(content)
+
+
 @app.route('/deencrypt/', methods=['GET', 'POST'])
 def deencrypt():
-    form = DeencryptForm(request.form)
+    form = DeencryptForm(CombinedMultiDict((request.files, request.form)))
+    if request.method == 'POST':
+        result = process_form(form)
+        if result:
+            tf = tempfile.NamedTemporaryFile()
+            with open(tf.name, 'wb') as out:
+                out.write(result)
+            return send_file(tf, attachment_filename='result.blob', as_attachment=True)
+
     return render_template('deencrypt.html', form=form)
 
 
--- test_app/ciphers.py
+++ test_app/ciphers.py
@@ -1,4 +1,3 @@
-import base64
 from enum import Enum
 
 from Crypto.Cipher import AES, XOR
@@ -15,33 +14,42 @@ __all__ = (
 )
 
 
-BS = 16
+def default_key():
+    return b'\x11\x94\xfb\xbc\xf1\xa3>pd\xf5\xa5\xcdd\xccSS'
 
-IV = Random.new().read(AES.block_size)  # b'\xb7\xc5(\xa7\xa8g\\\x1b_\\\x04=\x07\x87\x1a!'
+
+# IV = Random.new().read(AES.block_size)
+IV = b'\xb7\xc5(\xa7\xa8g\\\x1b_\\\x04=\x07\x87\x1a!'
 MODE_PCBC = 8
 
 
-def chunk_string(string, length: int = BS):
+def chunk_string(string, length: int = AES.block_size):
     yield from (string[0 + i:length + i] for i in range(0, len(string), length))
 
 
 class ChoiceEnum(Enum):
     @classmethod
     def choices(cls):
-        return [(choice.name, choice.name) for choice in cls]
+        return [(choice.name, choice.value) for choice in cls]
 
 
 class Ciphers(ChoiceEnum):
-    AES = AES.AESCipher
-    DSTU2014 = DSTU.DSTU2014Cipher
+    AES = 'AES'
+    Kalina = 'Kalina'
+
+
+ciphers = {
+    Ciphers.AES: AES.AESCipher,
+    Ciphers.Kalina: DSTU.DSTU2014Cipher,
+}
 
 
 class Modes(ChoiceEnum):
-    ECB = AES.MODE_ECB
-    CBC = AES.MODE_CBC
-    PCBC = MODE_PCBC
-    CFB = AES.MODE_CFB
-    OFB = AES.MODE_OFB
+    ECB = 'ECB'
+    CBC = 'CBC'
+    PCBC = 'PCBC'
+    CFB = 'CFB'
+    OFB = 'OFB'
 
 
 class Deencrypter:
@@ -50,33 +58,38 @@ class Deencrypter:
         self.iv = iv
         self.mode = mode
         self.key = key
-        self.cipher = cipher.value(self.key, Modes.ECB.value)
+        self.cipher = ciphers[cipher](self.key, AES.MODE_ECB)
 
     def deencrypt(self, text, reverse=False):
         res_parts = []
         prop = self.iv
         for part in chunk_string(text):
-            print(part)
             res, prop = self.block_deencrypt(part, reverse, prop)
             res_parts.append(res)
         return b''.join(res_parts)
 
-    def block_deencrypt(self, text, reverse: bool = False, prop=None):
+    def block_deencrypt(self, text, reverse, prop=None):
         if self.mode == Modes.ECB:
             res = self.cipher.decrypt(text) if reverse else self.cipher.encrypt(text)
         elif self.mode == Modes.CBC:
             if reverse:
-                res = XOR.new(prop).encrypt(self.cipher.decrypt(text))
-                prop = text
+                res, prop = XOR.new(prop).encrypt(self.cipher.decrypt(text)), text
             else:
-                text = XOR.new(prop).encrypt(text)
-                res = prop = self.cipher.encrypt(text)
+                res = prop = self.cipher.encrypt(XOR.new(prop).encrypt(text))
         elif self.mode == Modes.PCBC:
-            res, prop = ..., ...
+            if reverse:
+                res = XOR.new(prop).encrypt(self.cipher.decrypt(text))
+            else:
+                res = self.cipher.encrypt(XOR.new(prop).encrypt(text))
+            prop = XOR.new(res).encrypt(text)
         elif self.mode == Modes.CFB:
-            res, prop = ..., ...
+            if reverse:
+                res, prop = XOR.new(text).encrypt(self.cipher.encrypt(prop)), text
+            else:
+                res = prop = XOR.new(text).encrypt(self.cipher.encrypt(prop))
         elif self.mode == Modes.OFB:
-            res, prop = ..., ...
+            prop = self.cipher.encrypt(prop)
+            res = XOR.new(prop).encrypt(text)
         else:
             raise ValueError('Incorrect mode')
 
@@ -85,14 +98,11 @@ class Deencrypter:
     def encrypt(self, raw):
         if isinstance(raw, str):
             raw = bytes(raw, 'utf-8')
-        print(raw)
-        pad = BS - len(raw) % BS
-        print(raw + pad * pad.to_bytes(1, 'big'))
+        pad = AES.block_size - len(raw) % AES.block_size
         return self.deencrypt(raw + pad * pad.to_bytes(1, 'big'))
 
     def decrypt(self, enc):
         if isinstance(enc, str):
             enc = bytes(enc, 'utf-8')
         raw = self.deencrypt(enc, reverse=True)
-        print(raw)
         return raw[:-ord(raw[len(raw) - 1:])]
